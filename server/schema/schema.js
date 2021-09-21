36567083c5c2a921c668718643144425e9414d7c
Burak Özkan <burak.ozkan@path.com.tr>
2021-09-21 23:40:08 +0300

    fix server

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,16 +9,62 @@ const {
 } = graphql;
 
 let films = [
-  {id: '1', name: "Film 1", kind: "tür 1" },
-  {id: '2', name: "Film 2", kind: "tür 2" },
-  {id: '3', name: "Film 3", kind: "tür 1" },
+  {id: '1', name: "Film 1", kindId:"tür 1" },
+  {id: '2', name: "Film 2", kindId: "tür 2" },
+  {id: '3', name: "Film 3", kindId: "tür 1" },
+]
+
+let kinds = [
+  {id: '1', kindName: "tür 1" },
+  {id: '2', kindName: "tür 2" },
 ]
 
 const FilmType = new GraphQLObjectType({
   name: 'Film',
   fields: ()=> ({
     id: {type: GraphQLID},
-    name: {type: GraphQLString},
-    kind: {type: GraphQLString}
-  })
+    name: {type: GraphQLString}, 
+    kind: {
+      type: KindType,
+      resolve(parent, args){
+        console.log(parent)
+      }
+    }
+  }),
+});
+
+const KindType = new GraphQLObjectType({
+  name: 'Kind',
+  fields: ()=> ({
+    id: {type: GraphQLID},
+    kindName: {type: GraphQLString}
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'Root',
+  fields: {
+    film: {
+      type: FilmType,
+      args: {id:{type: GraphQLID}},
+      resolve(parent, args){
+        //db den gelecek
+  
+        return _.find(filmler, {id: args.id})
+      }
+    },
+    kind: {
+      type: KindType,
+      args: {id:{type: GraphQLID}},
+      resolve(parent, args){
+        //db den gelecek
+  
+        return _.find(kinds, {id: args.id})
+      }
+    }
+  }
+});
+
+export let schema = new GraphQLSchema({
+  query: RootQuery
 })
